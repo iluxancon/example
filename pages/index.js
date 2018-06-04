b55ae3e65402c8e4bc724a8b30d2710bab64a703
unknown <illiapopov2001@gmail.com>
2018-06-04 12:01:00 +0300

    mark2

--- pages/index.js
+++ pages/index.js
@@ -4,44 +4,44 @@ class ButtonWithCounter extends React.Component {
   }*/
 
   render() {
-    return <>
-      <h1>{this.props.title}</h1>
-      <button onClick={this.props.onChange}>
-        +
-      </button>
-      <span>{this.props.counter}</span>
-    </>
+    return (
+      <>
+        <h1>{this.props.title}</h1>
+        <button onClick={this.props.onChange}>+</button>
+        <span>{this.props.counter}</span>
+      </>
+    );
   }
 }
 
 export default class extends React.Component {
   state = {
-    "counter": 0,
-    "counter2": 100,
-    "commonCounter": 10,
-  }
+    counter: 0,
+    counter2: 100,
+    commonCounter: 10
+  };
 
   handleOnCounterClick = () => {
     this.setState({
-      "counter": this.state.counter + 1
-    })
-    alert("Привет, ты нажал на меня")
-  }
+      counter: this.state.counter + 1
+    });
+    alert("Привет, ты нажал на меня");
+  };
 
   handleOnCounter2Click = () => {
     this.setState({
-      "counter2": this.state.counter2 - 1
-    })
-  }
+      counter2: this.state.counter2 - 1
+    });
+  };
 
   handleOnChange = () => {
-    this.setState({ "commonCounter": this.state.commonCounter + 1 })
-  }
+    this.setState({ commonCounter: this.state.commonCounter + 1 });
+  };
 
   render() {
     return (
       <>
-                  <style>{`
+        <style>{`
                     h1,button {
                         background: blue;
                        //background: #FF0000;
@@ -51,10 +51,10 @@ export default class extends React.Component {
                       font-weight: bold;
                     }
                 `}</style>
-      <p>
-        <a href="/contests">Go to contests!</a>
-      </p>
-      <a href = "/page2">Page2</a>
+        <p>
+          <a href="/contests">Go to contests!</a>
+        </p>
+        <a href="/page2">Page2</a>
         <h1>
           Привет!
           <i>наклонный текст</i>
@@ -68,9 +68,8 @@ export default class extends React.Component {
         <ButtonWithCounter
           onChange={this.handleOnChange}
           counter={this.state.commonCounter}
-        ></ButtonWithCounter>
-        
+        />
       </>
-    )
+    );
   }
 }
